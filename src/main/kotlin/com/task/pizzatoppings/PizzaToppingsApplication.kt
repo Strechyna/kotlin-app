73991cd07bb849d091d4c2722092834519bae5d6
Valeryia Strechyna <ValeryiaStrechyna@coherentsolutions.com>
2023-04-26 12:01:45 +0200

    create project

---
+++ src/main/kotlin/com/task/pizzatoppings/PizzaToppingsApplication.kt
@@ -0,0 +1,11 @@
+package com.task.pizzatoppings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PizzaToppingsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PizzaToppingsApplication>(*args)
+}
